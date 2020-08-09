c3d7f24f81f72b79e65c8a9a6b4c62b1d7cee4b7
Sone <sthaya@gmail.com>
2020-08-09 12:29:24 -0700

    added images and created Home component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Home from './components/index'
 
 import './App.css';
 
 function App() {
   return (
     <>
-      <h1>hello</h1>
+      <Home />
     </>
   );
 }
