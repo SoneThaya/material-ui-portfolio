063451d0c4250dc09288808c79b142a0abb6133c
Sone <sthaya@gmail.com>
2020-08-11 19:27:11 -0700

    styled resume timeline

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -31,6 +31,74 @@ const useStyles = makeStyles(theme => ({
         right: "auto"
       }
     }
+  },
+  timeLineItem: {
+    padding: "1rem",
+    borderBottom: "2px solid tan",
+    position: "relative",
+    margin: "1rem 3rem 1rem 1rem",
+    clear: "both",
+    "&:after": {
+      content: "''",
+      position: "absolute"
+    },
+    "&:before": {
+      content: "''",
+      position: "absolute",
+      right: "-0.625rem",
+      top: "calc(50% - 5px)",
+      borderStyle: "solid",
+      borderColor: "tomato tomato transparent transparent",
+      borderWidth: "0.625rem",
+      transform: "rotate(45deg)"
+    },
+    [theme.breakpoints.up("md")]: {
+      width: "44%",
+      margin: "1rem",
+      "&:nth-of-type(2n)": {
+        float: "right",
+        margin: "1rem",
+        borderColor: "tan"
+      },
+      "&:nth-of-type(2n):before": {
+        right: "auto",
+        left: "-0.625rem",
+        borderColor: "transparent transparent tomato tomato"
+      }
+    }
+  },
+  timeLineYear: {
+    textAlign: "center",
+    maxWidth: "9.375rem",
+    margin: "0 3rem 0 auto",
+    background: "tomato",
+    color: "white",
+    lineHeight: 1,
+    padding: "0.5rem 0 1rem",
+    "&:before": {
+      display: "none"
+    },
+    [theme.breakpoints.up("md")]: {
+      textAlign: "center",
+      margin: "0 auto",
+      "&:nth-of-type(2n)": {
+        float: "none",
+        margin: "0 auto"
+      },
+      "&:nth-of-type(2n):before": {
+        display: "none"
+      }
+    }
+  },
+  heading: {
+    color: "tomato",
+    padding: "3rem 0",
+    textTransform: "uppercase"
+  },
+  subHeading: {
+    color: "white",
+    padding: "0",
+    textTransform: "uppercase"
   }
 }))
 
@@ -41,13 +109,84 @@ const Resume = () => {
     <>
       <Navbar />
       <Box component="header" className={classes.mainContainer}>
-        <Typography variant="h4" align="center">
+        <Typography variant="h4" align="center" className={classes.heading}>
           Working experience
         </Typography>
         <Box component="div" className={classes.timeLine}>
-          <Typography variant="h2">
+          <Typography variant="h2" className={`${classes.timeLineYear} ${classes.timeLineItem}`}>
             2013
           </Typography>
+          <Box component="div" className={classes.timeLineItem}>
+            <Typography variant="h5" align="center" className={classes.subHeading}>
+              company name where worked
+            </Typography>
+            <Typography variant="body1" align="center" style={{color: "tomato"}}>
+              web design
+            </Typography>
+            <Typography variant="subtitle1" align="center" style={{color: "tan"}}>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus fuga doloremque reprehenderit, odit laborum quam excepturi dolorem inventore nisi animi!
+            </Typography>
+          </Box>
+
+          <Typography variant="h2" className={`${classes.timeLineYear} ${classes.timeLineItem}`}>
+            2014
+          </Typography>
+          <Box component="div" className={classes.timeLineItem}>
+            <Typography variant="h5" align="center" className={classes.subHeading}>
+              company name where worked
+            </Typography>
+            <Typography variant="body1" align="center" style={{color: "tomato"}}>
+              html css
+            </Typography>
+            <Typography variant="subtitle1" align="center" style={{color: "tan"}}>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus fuga doloremque reprehenderit, odit laborum quam excepturi dolorem inventore nisi animi!
+            </Typography>
+          </Box>
+
+          <Typography variant="h2" className={`${classes.timeLineYear} ${classes.timeLineItem}`}>
+            2015
+          </Typography>
+          <Box component="div" className={classes.timeLineItem}>
+            <Typography variant="h5" align="center" className={classes.subHeading}>
+              company name where worked
+            </Typography>
+            <Typography variant="body1" align="center" style={{color: "tomato"}}>
+              Javascript Full Stack
+            </Typography>
+            <Typography variant="subtitle1" align="center" style={{color: "tan"}}>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus fuga doloremque reprehenderit, odit laborum quam excepturi dolorem inventore nisi animi!
+            </Typography>
+          </Box>
+
+          <Typography variant="h2" className={`${classes.timeLineYear} ${classes.timeLineItem}`}>
+            2017
+          </Typography>
+          <Box component="div" className={classes.timeLineItem}>
+            <Typography variant="h5" align="center" className={classes.subHeading}>
+              company name where worked
+            </Typography>
+            <Typography variant="body1" align="center" style={{color: "tomato"}}>
+              web design
+            </Typography>
+            <Typography variant="subtitle1" align="center" style={{color: "tan"}}>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus fuga doloremque reprehenderit, odit laborum quam excepturi dolorem inventore nisi animi!
+            </Typography>
+          </Box>
+
+          <Typography variant="h2" className={`${classes.timeLineYear} ${classes.timeLineItem}`}>
+            2019
+          </Typography>
+          <Box component="div" className={classes.timeLineItem}>
+            <Typography variant="h5" align="center" className={classes.subHeading}>
+              company name where worked
+            </Typography>
+            <Typography variant="body1" align="center" style={{color: "tomato"}}>
+              web design
+            </Typography>
+            <Typography variant="subtitle1" align="center" style={{color: "tan"}}>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus fuga doloremque reprehenderit, odit laborum quam excepturi dolorem inventore nisi animi!
+            </Typography>
+          </Box>
         </Box>
       </Box>
     </>
