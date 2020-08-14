52747da0d62b603bdbc6153e21fe8b6723313861
Sone <sthaya@gmail.com>
2020-08-13 17:09:36 -0700

    added more cards

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -13,6 +13,9 @@ import {
 } from '@material-ui/core'
 import Navbar from './Navbar'
 import project1 from "../images/html-css-javascript-lg.jpg"
+import project2 from "../images/javascript-fullstack.jpg"
+import project3 from "../images/react-redux.jpg"
+import project4 from "../images/mern-stack.jpg"
 
 
 const useStyles = makeStyles({
@@ -34,7 +37,7 @@ const Portfolio = () => {
       <Navbar />
       <Grid container justify="center">
         {/* Project 1 */}
-        <Grid>
+        <Grid item xs={12} sm={8} md={6}>
           <Card className={classes.cardContainer}>
             <CardActionArea>
               <CardMedia
@@ -42,7 +45,7 @@ const Portfolio = () => {
                 alt="Project 1"
                 height="140"
                 image={project1}
-              >
+              />
                 <CardContent>
                   <Typography gutterBottom variant="h5">
                     Project 1
@@ -51,6 +54,7 @@ const Portfolio = () => {
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, culpa hic. Illo quos excepturi aliquam nulla natus maxime a similique non, placeat quisquam incidunt, animi fugiat unde eaque doloremque! Repudiandae.
                   </Typography>
                 </CardContent>
+            </CardActionArea>
                 <CardActions>
                   <Button size="small" color="primary">
                     Share
@@ -59,8 +63,95 @@ const Portfolio = () => {
                     Share
                   </Button>
                 </CardActions>
-              </CardMedia>
+          </Card>
+        </Grid>
+        {/* Project 2 */}
+        <Grid item xs={12} sm={8} md={6}>
+          <Card className={classes.cardContainer}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                alt="Project 1"
+                height="140"
+                image={project2}
+              />
+                <CardContent>
+                  <Typography gutterBottom variant="h5">
+                    Project 2
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary" component="p">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, culpa hic. Illo quos excepturi aliquam nulla natus maxime a similique non, placeat quisquam incidunt, animi fugiat unde eaque doloremque! Repudiandae.
+                  </Typography>
+                </CardContent>
             </CardActionArea>
+                <CardActions>
+                  <Button size="small" color="primary">
+                    Share
+                  </Button>
+                  <Button size="small" color="primary">
+                    Share
+                  </Button>
+                </CardActions>
+          </Card>
+        </Grid>
+
+        {/* Project 3 */}
+        <Grid item xs={12} sm={8} md={6}>
+          <Card className={classes.cardContainer}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                alt="Project 1"
+                height="140"
+                image={project3}
+              />
+                <CardContent>
+                  <Typography gutterBottom variant="h5">
+                    Project 3
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary" component="p">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, culpa hic. Illo quos excepturi aliquam nulla natus maxime a similique non, placeat quisquam incidunt, animi fugiat unde eaque doloremque! Repudiandae.
+                  </Typography>
+                </CardContent>
+            </CardActionArea>
+                <CardActions>
+                  <Button size="small" color="primary">
+                    Share
+                  </Button>
+                  <Button size="small" color="primary">
+                    Share
+                  </Button>
+                </CardActions>
+          </Card>
+        </Grid>
+
+        {/* Project 4 */}
+        <Grid item xs={12} sm={8} md={6}>
+          <Card className={classes.cardContainer}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                alt="Project 1"
+                height="140"
+                image={project4}
+              />
+                <CardContent>
+                  <Typography gutterBottom variant="h5">
+                    Project 4
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary" component="p">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, culpa hic. Illo quos excepturi aliquam nulla natus maxime a similique non, placeat quisquam incidunt, animi fugiat unde eaque doloremque! Repudiandae.
+                  </Typography>
+                </CardContent>
+            </CardActionArea>
+                <CardActions>
+                  <Button size="small" color="primary">
+                    Share
+                  </Button>
+                  <Button size="small" color="primary">
+                    Share
+                  </Button>
+                </CardActions>
           </Card>
         </Grid>
       </Grid>
