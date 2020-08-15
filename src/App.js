a0e0d98404000094069e5d8093a98ac9acb646ea
Sone <sthaya@gmail.com>
2020-08-14 19:53:24 -0700

    created contacts component, styled

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './components/index'
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Resume from './components/Resume'
 import Portfolio from './components/Portfolio'
+import Contacts from './components/Contacts'
 
 import './App.css';
 
@@ -14,6 +15,7 @@ function App() {
       <Route exact path="/" component={Home} />
       <Route path="/resume" component={Resume} />
       <Route path="/portfolio" component={Portfolio} />
+      <Route path="/contacts" component={Contacts} />
     </>
   );
 }
